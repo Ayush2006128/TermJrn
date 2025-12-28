c55943c07573bb4c4ee6ce8bd5ce6640d9a34e61
Ayush Muley <74146837+Ayush2006128@users.noreply.github.com>
2025-12-28 11:08:52 +0530

    Implement file saving logic in FileOperations class
    Added TermJrn.lib namespace with a static FileOperations class. Implemented SaveNotesToFile method to write notes to a file and handle exceptions with error messages.

--- TermJrn/lib/FileOperations.cs
+++ TermJrn/lib/FileOperations.cs
@@ -1,1 +1,19 @@
-// TODO: Write logic to save notes to a file
+namespace TermJrn.lib
+{
+    using System;
+    using System.IO;
+    public static class FileOperations
+    {
+        public static void SaveNotesToFile(string filePath, string notes)
+        {
+            try
+            {
+                File.WriteAllText(filePath, notes);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while saving notes: {ex.Message}");
+            }
+        }
+    }
+}
